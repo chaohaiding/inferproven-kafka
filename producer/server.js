30a0ae83674d94c93552b7e6bcdec25c658cf72f
C.Ding <cd8e10@srv03260.soton.ac.uk>
2021-06-16 12:02:14 +0100

    dev version

---
+++ producer/server.js
@@ -0,0 +1,119 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const kafka = require('./kafka')
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+
+// kafka producer;
+const producer = kafka.producer()
+
+
+const main = async () => {
+  const admin = kafka.admin()
+  await admin.connect()
+
+  await producer.connect()
+  await admin.createTopics({
+    waitForLeaders: true,
+    topics: [{
+      topic: process.env.TOPIC
+    }, ],
+  })
+
+  // Tell express to use body-parser's JSON parsing
+  app.use(bodyParser.json())
+
+
+  let preNode = {};
+  // Set up a hook router for traffic data notification
+  app.post("/hook", async (req, res) => {
+    //console.log(req.body) // sensor data
+    const event = req.body;
+
+    // pre node exist?
+    if (Object.keys(preNode).length !== 0) {
+      /*
+      {
+        status: 'OK',
+        avgMeasuredTime: '66',
+        avgSpeed: '56',
+        extID: '668',
+        medianMeasuredTime: '66',
+        TIMESTAMP: '2014-02-13T11:30:00',
+        vehicleCount: '7',
+        _id: '190000',
+        REPORT_ID: '158324'
+      }
+      */
+
+      const node1 = {
+        id: preNode._id,
+        vehicleCount: preNode.vehicleCount,
+        avgMeasuredTime: preNode.avgMeasuredTime,
+        medianMeasuredTime: preNode.medianMeasuredTime,
+        extID: preNode.extID,
+        avgSpeed: preNode.avgSpeed,
+        REPORT_ID: preNode.REPORT_ID,
+        TIMESTAMP: preNode.TIMESTAMP
+      }
+
+      const node2 = {
+        id: event._id,
+        vehicleCount: event.vehicleCount,
+        avgMeasuredTime: event.avgMeasuredTime,
+        medianMeasuredTime: event.medianMeasuredTime,
+        extID: event.extID,
+        avgSpeed: event.avgSpeed,
+        REPORT_ID: event.REPORT_ID,
+        TIMESTAMP: event.TIMESTAMP
+      }
+
+      const value = {
+        "node1": node1,
+        "edge": event.TIMESTAMP,
+        "node2": node2,
+      }
+      /*
+      const value = {
+        "nodes":[node1, node2],
+        "edge": event.TIMESTAMP,
+      }
+      */
+      try {
+        const responses = await producer.send({
+          topic: process.env.TOPIC,
+          messages: [{
+            // _id as key, to make sure that we process events in order
+            key: event._id,
+            // The message value is just bytes to Kafka, so we need to serialize our JavaScript
+            // object to a JSON string. Other serialization methods like Avro are available.
+            value: JSON.stringify(value)
+          }]
+        });
+
+        console.log('Published message', {
+          responses
+        })
+      } catch (error) {
+        console.error('Error publishing message', error)
+      }
+
+
+    }
+    preNode = event;
+
+
+
+    res.status(200).end() // Responding is important
+  });
+
+  // Start express on the defined port
+  app.listen(PORT, () => console.log(`🚀 Producer server running on port ${PORT}`))
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
