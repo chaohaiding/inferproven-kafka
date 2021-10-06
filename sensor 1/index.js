24dfd5a25ad8d36a16e8025612187191587ca475
Chaohai Ding <chaohaiding@hotmail.co.uk>
2021-10-06 13:58:13 +0100

    update
    update

---
+++ sensor 1/index.js
@@ -0,0 +1,72 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const async = require("async");
+const http = require("http");
+const intervalTime = process.env.INTERVAL || 5 * 60 * 1000; // defualt 5 mintes
+
+const options = {
+  hostname: "127.0.0.1",
+  port: 3000,
+  path: "/hook",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const notify = (message) => {
+  const req = http.request(options, (res) => {
+    console.log(message);
+  });
+  req.on("error", function (e) {
+    console.log("Problem with request: " + e.message);
+  });
+  //send the whole traffic data to the hook
+  req.write(JSON.stringify(message));
+  req.end();
+};
+
+let result = [];
+// trafficData158324.csv
+// const fileName = "data1.csv";
+// const fileName = "trafficData158324.csv";
+fs.createReadStream("./data/" + fileName)
+  .pipe(csv())
+  .on("data", (row) => {
+    result.push(row);
+  })
+  .on("end", () => {
+    console.log(
+      "CSV file:" +
+        fileName +
+        " successfully processed, total records:" +
+        result.length
+    );
+    console.log("Start to send notify data with time interval:" + intervalTime);
+    async.eachSeries(
+      result,
+      function (data, next) {
+        setTimeout(function () {
+          /*
+        data Example:
+          {
+          status: 'OK',
+          avgMeasuredTime: '53',
+          avgSpeed: '69',
+          extID: '668',
+          medianMeasuredTime: '53',
+          TIMESTAMP: '2014-06-09T05:25:00',
+          vehicleCount: '0',
+          _id: '14353465',
+          REPORT_ID: '158324'
+        }
+        */
+          notify(data);
+          next(); // don't forget to execute the callback!
+        }, intervalTime);
+      },
+      function () {
+        console.log("All done!");
+      }
+    );
+  });
